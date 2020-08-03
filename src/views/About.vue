104949df0612f38a92c2580d44c97bc94d80f658
Shodiq Muhammad <muhammadshodiq165@live.com>
2020-08-04 06:10:47 +0700

    :sparkles: new contributor card

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,17 +2,31 @@
   <div class="about">
     <v-main>
       <AboutCard />
+      <ContributorsCard v-bind:contributors="contributors" />
     </v-main>
   </div>
 </template>
 
 <script>
 import AboutCard from '../components/cards/AboutCard';
+import ContributorsCard from '../components/cards/ContributorsCard';
 
 export default {
   name : 'About',
   components : {
     AboutCard,
+    ContributorsCard,
+  },
+  computed: {
+    contributors() {
+      var vc = " " + this.$root.$t("voice-clip");
+      return [
+        {
+          name: "Toastie",
+          desc: "Yo, wake up. It's already morning" + vc
+        }
+      ]
+    }
   }
 }
 </script>
